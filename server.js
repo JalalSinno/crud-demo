a2f731e3a6f2f1255781ebf257eccb7a5664fc77
Jamil Sinno <jamil.sinno@gmail.com>
2022-07-23 01:45:40 -0300

    added update

--- server.js
+++ server.js
@@ -10,17 +10,54 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     .then(client => {
         console.log('Connected to Database')
         const db = client.db('star-wars-quotes')
+        const quotesCollection = db.collection('quotes')
+
+        app.set('view engine', 'ejs')
+
+        //MIDDLEWARE
+
         //body parser before CRUD ops
         app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(express.static('public'))
+        app.use(bodyParser.json())
 
+        //READ
         app.get('/', (req, res) => {
-            res.sendFile(__dirname + '/index.html')
+            quotesCollection.find().toArray()
+                .then(results => {
+                    console.log(results)
+                    res.render('index.ejs', { quotes: results })
+                })
+                .catch(err => console.log(err))
         })
 
+        //CREATE
         app.post('/quotes', (req, res) => {
-            console.log(req.body)
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    res.redirect('/')
+                })
+                .catch(err => console.log(err))
           })
-          
+        
+        //UPDATE
+        app.put('/quotes', (req, res) => {
+           quotesCollection.findOneAndUpdate(
+                {name: 'test'},
+                {
+                    $set:{
+                        name: req.body.name,
+                        quote: req.body.quote
+                    }
+                },
+                {
+                    upsert: true,
+                }
+           )
+           .then(result => res.json('Success'))
+           .catch(err => console.log(err))
+        })
+
         app.listen(3000, () => {
             console.log('listening on 3000')
         })
